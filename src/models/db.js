b92626853d61ac3926510f2226aaec4e4fa3b79f
aarongoso <116266055+aarongoso@users.noreply.github.com>
2025-11-26 19:23:48 +0000

    Secure branch now uses its own dedicated secure_blog.db database

--- src/models/db.js
+++ src/models/db.js
@@ -6,7 +6,7 @@ const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error('Failed to connect to DB', err);
   } else {
-    console.log('Connected to SQLite DB (insecure branch).');
+    console.log('Connected to SQLite DB (secure branch).');
   }
 });
 
